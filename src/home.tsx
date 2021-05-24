498b386d1ee3019ff6c113e1baee8bd7876ac5c9
Johan <johan@macair2020>
2021-05-24 12:18:28 +0200

    add lnik to examples

--- src/home.tsx
+++ src/home.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { title, github } from "./config";
 
 export default () => (
   <>
     <h1>{title}</h1>
+
+    <p>
+      <Link to={"/example"}>Examples</Link>
+    </p>
 
     <p>
       <a href={github.url}>
