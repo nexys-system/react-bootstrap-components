fd018c810a90e686583b00f45419b97271baa86f
Johan <johan@macair2020>
2021-05-24 12:16:13 +0200

    added exports

---
+++ src/components/button/index.ts
@@ -0,0 +1,8 @@
+import Actions from "./actions";
+import Btn from "./btn";
+import Dropdown from "./dropdown";
+import Group from "./group";
+import Link from "./link";
+import * as Type from "./type";
+
+export { Actions, Btn, Dropdown, Group, Link, Type };
---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import * as Buttons from "./button";
+import Card from "./card";
+import * as Form from "./form";
+import * as Tabs from "./tabs";
+import Icon from "./icon";
+import Loader from "./loader";
+import ProgressBar from "./progress-bar";
+import * as Type from "./type";
+import WLoader from "./w-loader";
+
+export { Buttons, Card, Form, Tabs, Icon, Loader, ProgressBar, Type, WLoader };
