36e38f0fc31d1273c6c5f5e506b4a2ac2fa0c0b7
John B <john@nexys.ch>
2021-09-10 00:27:38 +0200

    added exports

---
+++ src/components/headless/form/index.ts
@@ -0,0 +1,8 @@
+export * as Layout from "./layout";
+export * as Type from "./type";
+export * as Utils from "./utils";
+
+import Generic from "./generic";
+import InputUnit from "./input-unit";
+
+export { Generic, InputUnit };
---
+++ src/components/headless/index.ts
@@ -0,0 +1,7 @@
+export * as Form from "./form";
+export * as View from "./view";
+
+import Toggle from "./toggle";
+import WLoader from "./w-loader";
+
+export { Toggle, WLoader };
