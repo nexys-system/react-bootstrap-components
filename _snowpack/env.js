6de1887d0dee97cd4784b38746d632522a8e6ba4
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-10 10:00:21 +0000

    deploy: 232253a74f78299154eb6a3681c39dfc868ed695

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "f5cbc1d276e53533fcde740f9f8ebfc8767e5c90";
+export const SNOWPACK_PUBLIC_GIT_SHA = "232253a74f78299154eb6a3681c39dfc868ed695";
 export const SNOWPACK_PUBLIC_URL = "/react-bootstrap-components";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- components/headless/index.js
+++ components/headless/index.js
@@ -1,5 +1,7 @@
 export * as Form from "./form/index.js";
 export * as View from "./view/index.js";
-import Toggle from "./toggle.js";
 import WLoader from "./w-loader/index.js";
-export {Toggle, WLoader};
+import Delete from "./delete.js";
+import Toggle from "./toggle.js";
+import Toggle2 from "./toggle2.js";
+export {WLoader, Delete, Toggle, Toggle2};
