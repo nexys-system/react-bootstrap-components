7c90ca7cb2d41101f87192c1db441bd2a62c013c
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-10 12:27:41 +0000

    deploy: 875b418c2494abacf6c422a0fa129d2cd276ad26

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "d48f21a05bca39ea7080d0ce9228c5efbbb87fca";
+export const SNOWPACK_PUBLIC_GIT_SHA = "875b418c2494abacf6c422a0fa129d2cd276ad26";
 export const SNOWPACK_PUBLIC_URL = "/react-bootstrap-components";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- components/form/index.js
+++ components/form/index.js
@@ -1,4 +1,5 @@
 export * as Input from "./input/index.js";
+export * as Generic from "./generic/index.js";
 import Wrapper from "./wrapper.js";
 import Layout from "./layout.js";
 export {Wrapper, Layout};
