4f851c9b5073c17e2c86c484f2c8070a1110b0e9
John B <john@nexys.ch>
2021-09-10 13:18:57 +0200

    add exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,29 @@ export * as Form from "./form";
 export * as Tabs from "./tabs";
 export * as Type from "./type";
 export * as Headless from "./headless";
+export * as UtilsUi from "./utils-ui";
 
 import Card from "./card";
+import Detail from "./detail";
+import Delete from "./delete";
+import Edit from "./edit";
+import List from "./list";
+import Toggle from "./toggle";
+
 import Icon from "./icon";
 import Loader from "./loader";
 import ProgressBar from "./progress-bar";
 import WLoader from "./w-loader";
 
-export { Card, Icon, Loader, ProgressBar, WLoader };
+export {
+  Card,
+  Detail,
+  Delete,
+  Edit,
+  Icon,
+  List,
+  Loader,
+  ProgressBar,
+  Toggle,
+  WLoader,
+};
