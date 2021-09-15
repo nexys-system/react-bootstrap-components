3cf8bfd9dcb5629e479bff7603011c2a83a070be
John B <johan@nexys.ch>
2021-09-15 18:43:20 +0200

    Update footer.tsx

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { github, sha, version } from "../config";
+import { github, version } from "../config";
 
 export default () => (
   <footer className="h-10 px-10">
