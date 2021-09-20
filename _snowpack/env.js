acaf502020f04d1a183d8e6f0956243f7d7ca5c1
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-20 09:57:20 +0000

    deploy: cac2e0ea034be53c8de4e951951691ca3299eb69

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "fd4ea7b88a2bc37084e5b2da07b1f9cd5cb5966f";
+export const SNOWPACK_PUBLIC_GIT_SHA = "cac2e0ea034be53c8de4e951951691ca3299eb69";
 export const SNOWPACK_PUBLIC_URL = "/react-bootstrap-components";
 export const MODE = "development";
 export const NODE_ENV = "development";
---
+++ components/headless/crud/generic.js
@@ -0,0 +1,46 @@
+import React from "../../../_snowpack/pkg/react.js";
+import {Route, Switch} from "../../../_snowpack/pkg/react-router-dom.js";
+import AddGeneric from "../add.js";
+const AddRoute = (Form, urlPrefix, addLink, crudInsert) => {
+  if (!crudInsert) {
+    return /* @__PURE__ */ React.createElement(React.Fragment, null);
+  }
+  const Add = AddGeneric(crudInsert, Form, urlPrefix);
+  return /* @__PURE__ */ React.createElement(Route, {
+    exact: true,
+    path: addLink,
+    component: Add
+  });
+};
+const CrudGeneric = (ListGeneric, DetailGeneric, FormWDef) => (def, defDetail, viewFields, urlPrefix, crud, editTitle = "Edit", extras, showEditToggle = true) => {
+  const Form = FormWDef(defDetail);
+  return CrudGenericWForm(ListGeneric, DetailGeneric, Form)(def, viewFields, urlPrefix, crud, editTitle, extras, showEditToggle);
+};
+const CrudGenericWForm = (ListGeneric, DetailGeneric, Form) => (def, viewFields, urlPrefix, crud, editTitle = "Edit", extras, showEditToggle = true) => {
+  const addLink = urlPrefix + "/add";
+  const editLink = (childId) => urlPrefix + "/" + childId + "/edit";
+  const redirectUrl = urlPrefix;
+  const backUrl = urlPrefix;
+  const List = () => /* @__PURE__ */ React.createElement(ListGeneric, {
+    editLink,
+    addLink,
+    getData: crud.list,
+    def
+  });
+  const PreDetail = DetailGeneric(Form, viewFields, {update: crud.update, deleteById: crud.deleteById, detail: crud.detail}, redirectUrl, showEditToggle);
+  const Detail = () => /* @__PURE__ */ React.createElement(PreDetail, {
+    backUrl,
+    title: editTitle,
+    extras: extras ? extras.map(({Component}) => ({id}) => /* @__PURE__ */ React.createElement(Component, {
+      data: id
+    })) : void 0
+  });
+  return () => /* @__PURE__ */ React.createElement(Switch, null, AddRoute(Form, urlPrefix, addLink, crud.insert), /* @__PURE__ */ React.createElement(Route, {
+    path: editLink(":id"),
+    component: Detail
+  }), /* @__PURE__ */ React.createElement(Route, {
+    path: urlPrefix,
+    component: List
+  }));
+};
+export default CrudGeneric;
