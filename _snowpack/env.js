4c22d443f8c590af818820090e02dc16996500a2
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-25 13:14:36 +0000

    deploy: 8abcbf16f57ba92266ece214bad9557e7c23e11a

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "eff55142f088f222e7715fd6b9c8e55360b650b6";
+export const SNOWPACK_PUBLIC_GIT_SHA = "8abcbf16f57ba92266ece214bad9557e7c23e11a";
 export const SNOWPACK_PUBLIC_URL = "/react-bootstrap-components";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- components/form/input/number.js
+++ components/form/input/number.js
@@ -4,13 +4,15 @@ export const InputNumber = ({
   onChange,
   errors,
   disabled,
-  placeholder
+  placeholder,
+  step
 }) => /* @__PURE__ */ React.createElement("input", {
   className: "form-control" + (errors && errors.length > 0 ? "  is-invalid" : ""),
   disabled,
   placeholder,
   type: "number",
   value: isNaN(Number(value)) ? "" : value,
+  step,
   onChange: (v) => v.target.value === "" ? onChange(void 0) : onChange(Number(v.target.value))
 });
 export default InputNumber;
