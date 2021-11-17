dcbe490ca67092cfa0610ceff8cc6f3539ca8092
John B <johan@nexys.ch>
2021-11-18 00:19:26 +0100

    Update type.ts

--- src/components/headless/list/type.ts
+++ src/components/headless/list/type.ts
@@ -1,8 +1,8 @@
 import { Types } from "@nexys/core-list";
-export interface ListProps<A> {
+export interface ListProps<A, Id> {
   getData: () => Promise<A[]>;
   addLink?: string;
-  editLink?: (id: number) => string;
+  editLink?: (id: Id) => string;
   def: Types.Definition<A>;
   config?: Types.Config<A>;
 }
