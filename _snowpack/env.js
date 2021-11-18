3c39083cb775acaf06e4fca7fdb4a3ce4b08ec00
johnb8005 <johnb8005@users.noreply.github.com>
2021-11-18 00:37:31 +0000

    deploy: ba0ecf01a5ee7317920fa4637f3f46d57fcea2c8

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "f9cb7f890ca4b0d00adb5ae72df5803a4002b0f2";
+export const SNOWPACK_PUBLIC_GIT_SHA = "ba0ecf01a5ee7317920fa4637f3f46d57fcea2c8";
 export const SNOWPACK_PUBLIC_URL = "/react-bootstrap-components";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- components/headless/detail/utils.js
+++ components/headless/detail/utils.js
@@ -1,13 +1,6 @@
-export const getParamTyped = (paramType, id) => {
+export const getParamTyped = (_paramType, id) => {
   if (!id) {
     throw Error("id could not be found");
-  }
-  if (paramType === "number") {
-    const nid = Number(id);
-    if (isNaN(nid)) {
-      throw Error("nid is not a number");
-    }
-    return nid;
   }
   return id;
 };
