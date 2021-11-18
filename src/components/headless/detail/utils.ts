57dd6320ecd6e83d93b425cfd5f8da876e6a02e7
John B <john@nexys.ch>
2021-11-18 01:36:46 +0100

    remove nid number

--- src/components/headless/detail/utils.ts
+++ src/components/headless/detail/utils.ts
@@ -1,18 +1,18 @@
 import { ParamType } from "./type";
 
-export const getParamTyped = <Id>(paramType: ParamType, id?: string): Id => {
+export const getParamTyped = <Id>(_paramType: ParamType, id?: string): Id => {
   if (!id) {
     throw Error("id could not be found");
   }
 
-  if (paramType === "number") {
+  /*if (paramType === "number") {
     const nid = Number(id);
     if (isNaN(nid)) {
       throw Error("nid is not a number");
     }
 
     return nid as any as Id;
-  }
+  }*/
 
   return id as any as Id;
 };
