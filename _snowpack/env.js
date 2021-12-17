e5849909a7ced0a0c71862b62c0c9e055bf651ad
johnb8005 <johnb8005@users.noreply.github.com>
2021-12-17 12:38:08 +0000

    deploy: 157e3be66ed6bde49bcfc8fc1e764846f9e72777

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "181a42254ac351e8852a5353af13430c8459e27f";
+export const SNOWPACK_PUBLIC_GIT_SHA = "157e3be66ed6bde49bcfc8fc1e764846f9e72777";
 export const SNOWPACK_PUBLIC_URL = "/react-bootstrap-components";
 export const MODE = "development";
 export const NODE_ENV = "development";
